ab0a3d19944a31c0af041284b86a4c60e3c5f410
kavy7884 <chingkaichou@gmail.com>
2015-02-13 18:52:54 +0800

    Create User OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
+	#User Gravtar picture
+	include Gravtastic
+  	gravtastic :secure => true,
+              :filetype => :png,
+              :size => 120
+	#Password
 	has_secure_password
+	#Validation
 	validates :email, presence: true, format:{with: /\A[^@]+@[^@]+\z/}
 	validates :name, presence: true, length:{maximum: 30}
 end
