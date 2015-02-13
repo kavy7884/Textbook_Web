62d4cdfc8a1956974948bd58d7e9e300b82d091d
kavy7884 <chingkaichou@gmail.com>
2015-02-13 16:41:13 +0800

    Correct wordtyping maximum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	validates :email, presence: true, format:{with: /\A[^@]+@[^@]+\z/}
-	validates :name, presence: true, length:{maximun: 30}
+	validates :name, presence: true, length:{maximum: 30}
 end
