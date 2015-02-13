891a27916a96ccfd19a64262b60fbfb874e0ec9b
kavy7884 <chingkaichou@gmail.com>
2015-02-13 16:03:07 +0800

    Add password and validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	has_secure_password
+	validates :email, presence: true, format:{with: /\A[^@]+@[^@]+\z/}
+	validates :name, presence: true, length:{maximun: 30}
 end
