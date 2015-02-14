2a560128c3d8bff4d737ce81dd7e09313b19a6ad
kavy7884 <chingkaichou@gmail.com>
2015-02-15 01:07:37 +0800

    Correct typing

--- db/migrate/20150214152058_add_user_session_token.rb
+++ db/migrate/20150214152058_add_user_session_token.rb
@@ -1,6 +1,6 @@
 class AddUserSessionToken < ActiveRecord::Migration
   def change
-  	add_column :Users, :session_token, :string
-  	add_index :Users, :session_token, unique: true
+  	add_column :users, :session_token, :string
+  	add_index :users, :session_token, unique: true
   end
 end
